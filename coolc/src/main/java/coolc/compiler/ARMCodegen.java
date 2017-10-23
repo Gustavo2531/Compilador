fa4ef7e54fbaef6861c4031062f7d219b9267178
yuks22 <a01337580@itesm.mx>
2017-10-23 13:50:31 -0500

    start of dispatch

--- coolc/src/main/java/coolc/compiler/ARMCodegen.java
+++ coolc/src/main/java/coolc/compiler/ARMCodegen.java
@@ -19,6 +19,9 @@ import coolc.compiler.autogen.node.AMultExpr;
 import coolc.compiler.autogen.node.APlusExpr;
 import coolc.compiler.autogen.node.AStrExpr;
 import coolc.compiler.autogen.node.AWhileExpr;
+import coolc.compiler.autogen.node.AWhileExpr;
+import coolc.compiler.autogen.node.AAtExpr;
+import coolc.compiler.autogen.node.ACallExpr;
 import coolc.compiler.autogen.node.Node;
 import coolc.compiler.autogen.node.PExpr;
 import coolc.compiler.autogen.node.PFeature;
@@ -98,6 +101,23 @@ public class ARMCodegen implements CodegenFacade {
 			
 			lastResult = st.render();
 		}
+		@Override
+		public void outAAtExpr(AAtExpr node) {
+            ST st;
+			st = templateGroup.getInstanceOf("atExpr");			
+			
+            lastResult=st.render();
+
+        }
+		
+        @Override
+        public void outACallExpr(ACallExpr node){
+            ST st;
+			st = templateGroup.getInstanceOf("callExpr");			
+			
+            lastResult=st.render();
+
+        }
 		@Override
 		public void outALetDecl(ALetDecl node) {
 			// TODO Auto-generated method stub
