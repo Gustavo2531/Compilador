20a052b778b6c112656f62f448f9917428d7d5c6
Javier Abdul Córdoba Gándara <abdul@itesm.mx>
2017-11-04 00:49:56 -0600

    Start from here the semantic analysis.

--- coolc/src/main/java/coolc/compiler/CoolSemantic.java
+++ coolc/src/main/java/coolc/compiler/CoolSemantic.java
@@ -1,55 +1,46 @@
 package coolc.compiler;
 
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
+import coolc.compiler.autogen.node.AFormal;
+import coolc.compiler.autogen.node.AMethodFeature;
+import coolc.compiler.autogen.node.ANoExpr;
+import coolc.compiler.autogen.node.AStrExpr;
+import coolc.compiler.autogen.node.PFeature;
+import coolc.compiler.autogen.node.PFormal;
+import coolc.compiler.autogen.node.TObjectId;
+import coolc.compiler.autogen.node.TTypeId;
 import coolc.compiler.autogen.analysis.DepthFirstAdapter;
 import coolc.compiler.autogen.node.AAttributeFeature;
 import coolc.compiler.autogen.node.AClassDecl;
+import coolc.compiler.autogen.node.AIntExpr;
 import coolc.compiler.autogen.node.Node;
 import coolc.compiler.autogen.node.Start;
 import coolc.compiler.exceptions.SemanticException;
 import coolc.compiler.util.Error;
 
 public class CoolSemantic implements SemanticFacade {
-	private Map<Node, AClassDecl> tipos;
+	private Map<Node, AClassDecl> types;
 	private Start start;
 	private PrintStream out;
 	
-	class ErrorsVisitor extends DepthFirstAdapter {
-		@Override
-		public void outAAttributeFeature(AAttributeFeature node) {
-			if (node.getObjectId().getText().equals("self")) {
-				ErrorManager.getInstance().getErrors().add(Error.SELF_ATTR);
-				ErrorManager.getInstance().semanticError("Coolc.semant.selfAttr");
-			}
-		}
-		
-		@Override
-		public void outAClassDecl(AClassDecl node) {
-			if (node.getName().getText().equals("Object")) {
-				ErrorManager.getInstance().getErrors().add(Error.REDEF_BASIC);
-				ErrorManager.getInstance().semanticError("Coolc.semant.redefBasic", node.getName().getText());						
-			}
-		}
-		
-	}
 
 	@Override
 	public void setup(Start start, PrintStream out) {
 		this.start = start;
 		this.out = out;
-
 	}
 
 	@Override
 	public void check() throws SemanticException {
-		this.start.apply(new ErrorsVisitor());
-		
-		if (ErrorManager.getInstance().getErrors().size() > 0) {
-			throw new SemanticException();
-		}
+
 	}
 
 	@Override
@@ -59,7 +50,215 @@ public class CoolSemantic implements SemanticFacade {
 
 	@Override
 	public Map<Node, AClassDecl> getTypes() {
-		return this.tipos;
+		return this.types;
+	}
+	
+	public Map<String, AClassDecl> basicClasses() {
+		// The following demonstrates how to create dummy parse trees to
+		// refer to basic Cool classes. There's no need for method
+		// bodies -- these are already built into the runtime system.
+
+		// IMPORTANT: The results of the following expressions are
+		// stored in local variables. You will want to do something
+		// with those variables at the end of this method to make this
+		// code meaningful.
+
+		// The Object class has no parent class. Its methods are
+		// cool_abort() : Object aborts the program
+		// type_name() : Str returns a string representation
+		// of class name
+		// copy() : SELF_TYPE returns a copy of the object
+		LinkedList<PFeature> featList;
+		LinkedList<PFormal> formalList;
+		PFeature pf;
+		
+		featList = new LinkedList<PFeature>();
+		
+		pf = new AMethodFeature(
+				new TObjectId("abort"),
+				new LinkedList<PFormal>(),
+				new TTypeId("Object"),
+				new ANoExpr()
+				);		
+		featList.add(pf);
+		
+		pf = new AMethodFeature(
+				new TObjectId("type_name"),
+				new LinkedList<PFormal>(),
+				new TTypeId("String"),
+				new ANoExpr()
+				);
+		featList.add(pf);
+		
+		pf = new AMethodFeature(
+				new TObjectId("copy"),
+				new LinkedList<PFormal>(),
+				new TTypeId("SELF_TYPE"),
+				new ANoExpr()
+				);
+		featList.add(pf);
+		
+		AClassDecl ObjectClass = new AClassDecl(
+				new TTypeId("Object"),
+				new TTypeId("_no_class"),
+				featList
+				);
+		
+		// The IO class inherits from Object. Its methods are
+		// out_string(Str) : SELF_TYPE writes a string to the output
+		// out_int(Int) : SELF_TYPE "    an int    " "     "
+		// in_string() : Str reads a string from the input
+		// in_int() : Int "   an int     " "     "
+		featList = new LinkedList<PFeature>();
+		
+		formalList = new LinkedList<PFormal>();		
+		formalList.add(new AFormal(new TObjectId("arg"), new TTypeId("String")));		
+		pf = new AMethodFeature(
+				new TObjectId("out_string"),
+				formalList,
+				new TTypeId("SELF_TYPE"),
+				new ANoExpr()
+				);		
+		featList.add(pf);
+
+		formalList = new LinkedList<PFormal>();		
+		formalList.add(new AFormal(new TObjectId("arg"), new TTypeId("Int")));
+		pf = new AMethodFeature(
+				new TObjectId("out_int"),
+				formalList,
+				new TTypeId("SELF_TYPE"),
+				new ANoExpr()
+				);		
+		featList.add(pf);
+
+		formalList = new LinkedList<PFormal>();
+		pf = new AMethodFeature(
+				new TObjectId("in_string"),
+				formalList,
+				new TTypeId("String"),
+				new ANoExpr()
+				);		
+		featList.add(pf);
+
+		pf = new AMethodFeature(
+				new TObjectId("in_int"),
+				formalList,
+				new TTypeId("Int"),
+				new ANoExpr()
+				);		
+		featList.add(pf);
+
+		AClassDecl IOClass = new AClassDecl(
+				new TTypeId("IO"),
+				new TTypeId("Object"),
+				featList
+				);
+
+
+		// The Int class has no methods and only a single attribute, the
+		// "val" for the integer.
+		featList = new LinkedList<PFeature>();
+		pf = new AAttributeFeature(
+				new TObjectId("_val"),
+				new TTypeId("_prim_slot"),
+				new ANoExpr()
+				);		
+		featList.add(pf);
+		
+		AClassDecl IntClass = new AClassDecl(
+				new TTypeId("Int"),
+				new TTypeId("Object"),
+				featList
+				);
+
+		// Bool also has only the "val" slot.
+		featList = new LinkedList<PFeature>();
+		pf = new AAttributeFeature(
+				new TObjectId("_val"),
+				new TTypeId("_prim_slot"),
+				new ANoExpr()
+				);		
+		featList.add(pf);
+		
+		AClassDecl BoolClass = new AClassDecl(
+				new TTypeId("Bool"),
+				new TTypeId("Object"),
+				featList
+				);
+
+		// The class Str has a number of slots and operations:
+		// val the length of the string
+		// str_field the string itself
+		// length() : Int returns length of the string
+		// concat(arg: Str) : Str performs string concatenation
+		// substr(arg: Int, arg2: Int): Str substring selection
+		featList = new LinkedList<PFeature>();
+		pf = new AAttributeFeature(
+				new TObjectId("_val"),
+				new TTypeId("_prim_slot"),
+				new ANoExpr()
+				);		
+		featList.add(pf);
+
+		pf = new AAttributeFeature(
+				new TObjectId("_str_field"),
+				new TTypeId("_prim_slot"),
+				new ANoExpr()
+				);		
+		featList.add(pf);
+		
+		pf = new AMethodFeature(
+				new TObjectId("length"),
+				new LinkedList<PFormal>(),
+				new TTypeId("Int"),
+				new ANoExpr()
+				);
+		featList.add(pf);		
+
+		formalList = new LinkedList<PFormal>();
+		formalList.add(new AFormal(new TObjectId("arg"), new TTypeId("String")));
+		pf = new AMethodFeature(
+				new TObjectId("concat"),
+				formalList,
+				new TTypeId("String"),
+				new ANoExpr()
+				);		
+		featList.add(pf);
+
+		formalList = new LinkedList<PFormal>();
+		formalList.add(new AFormal(new TObjectId("arg"), new TTypeId("Int")));
+		formalList.add(new AFormal(new TObjectId("arg2"), new TTypeId("Int")));
+		pf = new AMethodFeature(
+				new TObjectId("substr"),
+				formalList,
+				new TTypeId("String"),
+				new ANoExpr()
+				);		
+		featList.add(pf);
+		
+		
+		AClassDecl StringClass = new AClassDecl(
+				new TTypeId("String"),
+				new TTypeId("Object"),
+				featList
+				);
+		
+
+		/*
+		 * Do something with Object_class, IO_class, Int_class, Bool_class, and
+		 * Str_class here 
+		 * 
+		 */
+
+		Map<String, AClassDecl> map = new TreeMap<String, AClassDecl>();
+		map.put("Object", ObjectClass);
+		map.put("IO", IOClass);
+		map.put("Int", IntClass);
+		map.put("Bool", BoolClass);
+		map.put("String", StringClass);
+		
+		return map;
+		
 	}
 
 }
